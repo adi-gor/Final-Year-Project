8c255b3ed0a125f8255a4bb50500b384ab2de123
Aditya Gor <adityagor13@gmail.com>
2023-02-02 00:04:41 +0530

    Add files via upload

---
+++ sensordata/post-esp-data.php
@@ -0,0 +1,55 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "rellik";
+$dbname = "sensordata";
+
+/*$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);*/
+
+$api_key_value = "tPmAT5Ab3j7F9";
+
+$api_key= $sensor = $location = $value1 = $value2 = $value3 = "";
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $api_key = test_input($_POST["api_key"]);
+    if($api_key == $api_key_value) {
+        $sensor = test_input($_POST["sensor"]);
+        $location = test_input($_POST["location"]);
+        $value1 = test_input($_POST["value1"]);
+        $value2 = test_input($_POST["value2"]);
+        $value3 = test_input($_POST["value3"]);
+        
+        // Create connection
+        $conn = new mysqli($servername, $username, $password, $dbname);
+        // Check connection
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+        } 
+        
+        $sql = "INSERT INTO SensorData (sensor, location, value1, value2, value3)
+        VALUES ('" . $sensor . "', '" . $location . "', '" . $value1 . "', '" . $value2 . "', '" . $value3 . "')";
+        
+        if ($conn->query($sql) === TRUE) {
+            echo "New record created successfully";
+        } 
+        else {
+            echo "Error: " . $sql . "<br>" . $conn->error;
+        }
+    
+        $conn->close();
+    }
+    else {
+        echo "Wrong API Key provided.";
+    }
+
+}
+else {
+    echo "No data posted with HTTP POST.";
+}
+
+function test_input($data) {
+    $data = trim($data);
+    $data = stripslashes($data);
+    $data = htmlspecialchars($data);
+    return $data;
+}
