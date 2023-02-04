a1015f45d458dcca2d47cc9cca09b498ea4ac962
Aditya Gor <adityagor13@gmail.com>
2023-02-04 18:24:20 +0530

    Add files via upload

---
+++ sensordata/auth.js
@@ -0,0 +1,34 @@
+function login()
+	{
+		var uname = document.getElementById("email").value;
+		var pwd = document.getElementById("pwd1").value;
+		var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+		if(uname =='')
+		{
+			alert("please enter user name.");
+		}
+		else if(pwd=='')
+		{
+        	alert("enter the password");
+		}
+		else if(!filter.test(uname))
+		{
+			alert("Enter valid email id.");
+		}
+		else if(pwd.length < 6 || pwd.length > 6)
+		{
+			alert("Password min and max length is 6.");
+		}
+		else
+		{
+	alert('Redirecting to the Status Page');
+  //Redirecting to other page or webste code or you can set your own html page.
+       window.location = "http://localhost/sensordata/";
+			}
+	}
+	//Reset Inputfield code.
+	function clearFunc()
+	{
+		document.getElementById("email").value="";
+		document.getElementById("pwd1").value="";
+	}
---
+++ sensordata/index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta http-equiv="refresh" content="5" >
+    <link rel="stylesheet" type="text/css" href="style.css" media="screen"/>
+
+	<title> Sensor Data </title>
+
+</head>
+
+<body>
+
+    <h1>SENSOR DATA</h1>
+    <h3><a href="http://192.168.43.110/">Motor Status</a></h3>
+    <h3><a href="http://127.0.0.1:2222/">Crop Recommendation System</a></h3>
+    <h3><a href="http://localhost:3000/">Export CSV</a></h3>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "rellik";
+$dbname = "sensordata";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT id, sensor, location, value1, value2, value3, reading_time FROM sensordata ORDER BY id DESC"; /*select items to display from the sensordata table in the data base*/
+
+echo '<table cellspacing="5" cellpadding="5">
+      <tr> 
+        <th>ID</th> 
+        <th>Date & Time</th> 
+        <th>Sensor</thh> 
+        <th>Location</th> 
+        <th>Temperature</th> 
+        <th>Humidity</th>
+        <th>Soil Moisture</th>       
+      </tr>';
+ 
+if ($result = $conn->query($sql)) {
+    while ($row = $result->fetch_assoc()) {
+        $row_id = $row["id"];
+        $row_reading_time = $row["reading_time"];
+        $row_sensor = $row["sensor"];
+        $row_location = $row["location"];
+        $row_value1 = $row["value1"];
+        $row_value2 = $row["value2"]; 
+        $row_value3 = $row["value3"]; 
+        
+        // Uncomment to set timezone to - 1 hour (you can change 1 to any number)
+       // $row_reading_time = date("Y-m-d H:i:s", strtotime("$row_reading_time - 1 hours"));
+      
+        // Uncomment to set timezone to + 4 hours (you can change 4 to any number)
+        //$row_reading_time = date("Y-m-d H:i:s", strtotime("$row_reading_time + 4 hours"));
+        if ($row_value3 == '0')
+        {
+            $rain_value = "High";
+        }
+        if ($row_value3 == '1')
+        {
+            $rain_value = "Low";
+        }
+        echo '<tr> 
+                <td>' . $row_id . '</td> 
+                <td>' . $row_reading_time . '</td> 
+                <td>' . $row_sensor . '</td> 
+                <td>' . $row_location . '</td> 
+                <td>' . $row_value1 . '</td> 
+                <td>' . $row_value2 . '</td>
+                <td>' . $rain_value . '</td>
+              </tr>';
+    }
+    $result->free();
+}
+
+$conn->close();
+?> 
+</table>
+
+</body>
+</html>
+
+	</body>
+</html>
