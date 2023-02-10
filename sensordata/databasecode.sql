5c2b80d7fd5cb605c42d0422f9e088c2bfe44db3
Aditya Gor <adityagor13@gmail.com>
2023-02-10 17:18:27 +0530

    Update databasecode.sql

--- sensordata/databasecode.sql
+++ sensordata/databasecode.sql
@@ -2,11 +2,11 @@
 /*creating a table in the database*/
 CREATE TABLE sensordata (
   id int(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-  sensor varchar(30) not null,
-  location varchar(30) not null,
-  value1 varchar(10),
-  value2 varchar(10),
-  value3 varchar(10),
-  reading_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  SENSORS varchar(30) not null,
+  LOCATION varchar(30) not null,
+  TEMPERATURE varchar(10),
+  HUMIDITY varchar(10),
+  WATER_LEVEL varchar(10),
+  READING_TIME TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
