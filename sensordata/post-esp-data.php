87eb4608a66634438ab266db62e201287a91bb34
Aditya Gor <adityagor13@gmail.com>
2023-02-19 01:15:05 +0530

    Update post-esp-data.php

--- sensordata/post-esp-data.php
+++ sensordata/post-esp-data.php
@@ -26,7 +26,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             die("Connection failed: " . $conn->connect_error);
         } 
         
-        $sql = "INSERT INTO SensorData (SENSORS, LOCATION, TEMPERATURE, HUMIDITY, WATER_LEVEL)
+        $sql = "INSERT INTO SensorData (SENSORS, LOCATION, TEMPERATURE, HUMIDITY, SOIL_MOISTURE)
         VALUES ('" . $sensor . "', '" . $location . "', '" . $value1 . "', '" . $value2 . "', '" . $value3 . "')";
         
         if ($conn->query($sql) === TRUE) {
