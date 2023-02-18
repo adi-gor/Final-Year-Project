6a5d2dca6f9e6e75892a4d0617cd9bad5f7748d7
Aditya Gor <adityagor13@gmail.com>
2023-02-19 01:19:10 +0530

    Update index.js

--- sensordata/csv_export/routes/index.js
+++ sensordata/csv_export/routes/index.js
@@ -8,7 +8,7 @@ var data_exporter = require('json2csv').Parser;
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
-    database.query('SELECT sensor, location, value1, value2, value3, reading_time FROM sensordata', function(error, data){
+    database.query('SELECT SENSORS, LOCATION, TEMPERATURE, HUMIDITY, SOIL_MOISTURE, READING_TIME FROM sensordata', function(error, data){
 
         res.render('index', { title: 'Express', sensordata:data });
 
@@ -18,13 +18,13 @@ router.get('/', function(req, res, next) {
 
 router.get('/export', function(request, response, next){
 
-    database.query('SELECT sensor, location, value1, value2, value3, reading_time FROM sensordata', function(error, data){
+    database.query('SELECT SENSORS, LOCATION, TEMPERATURE, HUMIDITY, SOIL_MOISTURE, READING_TIME FROM sensordata', function(error, data){
 
         var mysql_data = JSON.parse(JSON.stringify(data));
 
         //convert JSON to CSV Data
 
-        var file_header = ['Sensor', 'Location', 'Value1', 'Value2', 'Value3', 'Reading_Time'];
+        var file_header = ['SENSORS', 'LOCATION', 'TEMPERATURE', 'HUMIDITY', 'SOIL_MOISTURE', 'READING_TIME'];
 
         var json_data = new data_exporter({file_header});
 
